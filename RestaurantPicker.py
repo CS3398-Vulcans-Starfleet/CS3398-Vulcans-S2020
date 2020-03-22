784638c9dcb3838b1e882ace70d4cbdfdb671750
cjs141 <60361799+cjs141@users.noreply.github.com>
2020-03-22 10:23:08 -0500

    Delete RestaurantPicker.py

--- RestaurantPicker.py
+++
@@ -1,7 +0,0 @@
-import random
-
-List = ["Las Flores", "Dry Creek", "Huts Hamburgers", "Tarka", "Opal Divine",
-"Hai Kei", "Rapolo", "Chez Eez", "NxNw", "Wild Bubbas", "Fish City", "Deckhands"]
-
-print("And the restaurant is: ", random.choice(List))
-9
